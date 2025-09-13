8fb95dafe80a04d96427a544a95c7dd331e3a833
David <David.Piruzashvili@gmail.com>
2025-09-13 23:20:18 +0400

    fix: minification chunk

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          antd: ['antd']
+        }
+      }
+    }
+  }
 })
